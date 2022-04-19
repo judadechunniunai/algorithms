f4298a5db9f0c66d888ab9af441a0876cb94ca28
judadechunniunai <asdfghjkl190226~>
2022-04-19 23:13:02 +0800

    哈哈1

--- java/src/Huiwen.java
+++ java/src/Huiwen.java
@@ -24,8 +24,6 @@ public class Huiwen {
     public int expandAroundCenter(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             --left;
-
-
             ++right;
         }
         return right - left - 1;
